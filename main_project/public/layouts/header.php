ebe3c30fa7789cf8859d86821885dec6a2b8dd84
swarup260 <swarup260@gmail.com>
2018-02-16 00:11:58 +0530

    login system complete and gitignore file added

--- main_project/public/layouts/header.php
+++ main_project/public/layouts/header.php
@@ -6,17 +6,17 @@
   <title>TECH SEVIN </title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="../../bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo rtrim($_SERVER['PHP_SELF'],"/index.php").'/bower_components/bootstrap/dist/css/bootstrap.min.css';?>">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="../../bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo rtrim($_SERVER['PHP_SELF'],"/index.php").'/bower_components/font-awesome/css/font-awesome.min.css'?>">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="../../bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo rtrim($_SERVER['PHP_SELF'],"/index.php").'/bower_components/Ionicons/css/ionicons.min.css'?>">
   <!-- Theme style -->
-  <link rel="stylesheet" href="../../bower_components/admin-lte/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="<?php echo rtrim($_SERVER['PHP_SELF'],"/index.php").'/bower_components/admin-lte/dist/css/AdminLTE.min.css'?>">
   <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
         page. However, you can choose any other skin. Make sure you
         apply the skin class to the body tag so the changes take effect. -->
-  <link rel="stylesheet" href="../../bower_components/admin-lte/dist/css/skins/skin-blue.min.css">
+  <link rel="stylesheet" href="<?php echo rtrim($_SERVER['PHP_SELF'],"/index.php").'/bower_components/admin-lte/dist/css/skins/skin-blue.min.css'?>">
   <!-- Google Font -->
   <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
